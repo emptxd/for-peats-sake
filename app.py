7f3e6c14df81cc5129972a5314d9f89fb64a6359
emptxd <155116181+emptxd@users.noreply.github.com>
2025-05-23 16:32:34 +0200

    update

--- app.py
+++ app.py
@@ -16,29 +16,17 @@ def what_is_paludiculture():
 def can_i_do_it():
     return render_template('can_i_do_it.html')
 
-@app.route('/biomass-uses')
-def biomass_uses():
-    return render_template('biomass_uses.html')
 
 @app.route('/success-stories')
 def success_stories():
     return render_template('success_stories.html')
 
-@app.route('/policymakers')
-def policymakers():
-    return render_template('policymakers.html')
-
-@app.route('/learn-more')
-def learn_more():
-    return render_template('learn_more.html')
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.route('/downloads')
-def downloads():
-    return render_template('downloads.html')
+
 
 
 # === MAIN ENTRY POINT ===
